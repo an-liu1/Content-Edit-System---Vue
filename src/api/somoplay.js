db6d793bc5556a3035d696746c7e50ae833f1164
an-liu1 <a_liu@fanshaweonline.ca>
2020-01-30 16:40:50 -0500

    a

--- src/api/somoplay.js
+++ src/api/somoplay.js
@@ -1,7 +1,8 @@
-import request from './request'
+import request from "./request";
 
-const getAPI = "http://159.89.121.159:3008/somoInit/searchSomoplayWebByPageAndSection";  //GET
-const otherAPI = "http://159.89.121.159:3008/somo/somoplayWeb";  //POST PUT DELETE
+const getAPI =
+  "http://159.89.121.159:3008/somoInit/searchSomoplayWebByPageAndSection"; //GET
+const otherAPI = "http://159.89.121.159:3008/somo/somoplayWeb"; //POST PUT DELETE
 const AuthAPI = "http://159.89.121.159:3008/auth/login";
 // const Register = "http://159.89.121.159:3008/auth/register";
 // {
@@ -12,26 +13,26 @@ const AuthAPI = "http://159.89.121.159:3008/auth/login";
 // }
 const imgAPI = "http://159.89.121.159:3008/somo/somoplayWeb/";
 
-export const getData = (address) => {
-    return request('get', getAPI + address)
-}
+export const getData = address => {
+  return request("get", getAPI + address);
+};
 
-export const newData = (params) => {
-    return request('post', otherAPI, params)    
-}
+export const newData = params => {
+  return request("post", otherAPI, params);
+};
 
-export const deleteData = (params) => {
-    return request('delete', otherAPI, params)    
-}
+export const deleteData = params => {
+  return request("delete", otherAPI, params);
+};
 
-export const editData = (params) => {
-    return request('put', otherAPI, params)    
-}
+export const editData = params => {
+  return request("put", otherAPI, params);
+};
 
-export const authData = (params) => {
-    return request('auth', AuthAPI, params)    
-}
+export const authData = params => {
+  return request("auth", AuthAPI, params);
+};
 
-export const getImg = (address) => {
-    return imgAPI + address
-}
+export const getImg = address => {
+  return imgAPI + address;
+};
